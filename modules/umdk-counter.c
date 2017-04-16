2ddb742167e77b8830e433966a17216fb49611c0
Oleg Artamonov <oleg@unwds.com>
2017-04-16 21:34:36 +0300

    Support for counter module commands

--- modules/umdk-counter.c
+++ modules/umdk-counter.c
@@ -36,8 +36,66 @@
 #include "unwds-modules.h"
 #include "utils.h"
 
+typedef enum {
+    UMDK_COUNTER_CMD_SET_PERIOD = 0,
+    UMDK_COUNTER_CMD_POLL = 1,
+    UMDK_COUNTER_CMD_RESET = 2,
+    UMDK_COUNTER_CMD_SET_INITIAL_VALUES = 3,
+    UMDK_COUNTER_CMD_SET_P2L_COEFF = 4,
+} umdk_counter_cmd_t;
+
 void umdk_counter_command(char *param, char *out, int bufsize)
 {
+    if (strstr(param, "period ") == param) {
+        param += strlen("period "); // skip command
+
+        uint8_t period = strtol(param, &param, 10);
+        printf("[mqtt-counter] Set period: %" PRIu8 " hrs\n", period);
+
+        snprintf(out, bufsize, "%02x%02x", UMDK_COUNTER_CMD_SET_PERIOD, period);
+    }
+    
+    if (strstr(param, "coeff ") == param) {
+        param += strlen("coeff "); // skip command
+
+        uint8_t coeff = strtol(param, &param, 10);
+        printf("[mqtt-counter] Set coefficient: %" PRIu8 " l/p\n", coeff);
+
+        snprintf(out, bufsize, "%02x%02x", UMDK_COUNTER_CMD_SET_P2L_COEFF, coeff);
+    }
+    
+    if (strstr(param, "reset") == param) {
+        snprintf(out, bufsize, "%02x", UMDK_COUNTER_CMD_RESET);
+    }
+    
+    if (strstr(param, "get") == param) {
+        snprintf(out, bufsize, "%02x", UMDK_COUNTER_CMD_POLL);
+    }
+    
+    if (strstr(param, "values ") == param) {
+        param += strlen("values "); // skip command
+        
+        uint32_t values[4];
+        for (int i = 0; i < 4; i++) {
+            values[i] = strtol(param, &param, 10);
+        }
+        
+        uint32_t v_compressed[3];
+        
+        v_compressed[0]  = c_conv[0] << 8;
+        v_compressed[0] |= (c_conv[1] >> 16) & 0xFF;
+        
+        v_compressed[1] = c_conv[1] << 16;
+        v_compressed[1] |= (c_conv[2] >> 8) & 0xFFFF;
+        
+        v_compressed[2] = (c_conv[2] << 24);
+        v_compressed[2] |= c_conv[3] & 0xFFFFFF;
+        
+        snprintf(out, bufsize, "%02x%08x%08x%08x",
+                    UMDK_COUNTER_CMD_SET_INITIAL_VALUES,
+                    v_compressed[0], v_compressed[1], v_compressed[2]);
+    }
+    
     return;
 }
 
@@ -77,12 +135,15 @@ bool umdk_counter_reply(uint8_t *moddata, int moddatalen, mqtt_msg_t *mqtt_msg)
     values[1] = ((num[0] & 0xFF) << 16) | (num[1] >> 16);
     values[2] = ((num[1] & 0xFFFF) << 8) | (num[2] >> 24);
     values[3] = num[2] & 0xFFFFFF;
-
+    
     for (i = 0; i < 4; i++) {   
         snprintf(ch, sizeof(ch), "v%d", i);
         snprintf(buf, sizeof(buf), "%u", values[i]);
         add_value_pair(mqtt_msg, ch, buf);
     }
+    
+    snprintf(buf, sizeof(buf), "%" PRIu8, moddata[12];
+    add_value_pair(mqtt_msg, "coeff", buf);
     
     return true;
 }
