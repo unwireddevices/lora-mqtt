96f12f37a905c5e75a8763ddad970ad7df9748b7
Oleg Artamonov <oleg@unwds.com>
2017-12-03 08:40:16 +0300

    umdk-irblaster initial implementation added

--- modules/include/unwds-modules.h
+++ modules/include/unwds-modules.h
@@ -24,7 +24,7 @@
  * @ingroup     
  * @brief       
  * @{
- * @file		unwds-modules.h
+ * @file        unwds-modules.h
  * @brief       functions to access unwds modules
  * @author      Oleg Artamonov
  */
@@ -63,6 +63,7 @@ bool umdk_iec61107_reply(uint8_t *moddata, int moddatalen, mqtt_msg_t *mqtt_msg)
 bool umdk_pacs_reply(uint8_t *moddata, int moddatalen, mqtt_msg_t *mqtt_msg);
 bool umdk_hd44780_reply(uint8_t *moddata, int moddatalen, mqtt_msg_t *mqtt_msg);
 bool umdk_idcard_reply(uint8_t *moddata, int moddatalen, mqtt_msg_t *mqtt_msg);
+bool umdk_irblaster_reply(uint8_t *moddata, int moddatalen, mqtt_msg_t *mqtt_msg);
 
 void umdk_counter_command(char *param, char *out, int bufsize);
 void umdk_pulse_command(char *param, char *out, int bufsize);
@@ -88,37 +89,39 @@ void umdk_iec61107_command(char *param, char *out, int bufsize);
 void umdk_pacs_command(char *param, char *out, int bufsize);
 void umdk_hd44780_command(char *param, char *out, int bufsize);
 void umdk_idcard_command(char *param, char *out, int bufsize);
+void umdk_irblaster_command(char *param, char *out, int bufsize);
 
 static const unwds_module_desc_t unwds_modules_list[] = {
-    { .id = UNWDS_GPIO_MODULE_ID,    .name = "gpio",    .cmd = &umdk_gpio_command,       .reply = &umdk_gpio_reply     },
-    { .id = UNWDS_4BTN_MODULE_ID,    .name = "4btn",    .cmd = NULL,                     .reply = &umdk_4btn_reply     },
-    { .id = UNWDS_COUNTER_MODULE_ID, .name = "counter", .cmd = &umdk_counter_command,    .reply = &umdk_counter_reply  },
-    { .id = UNWDS_GPS_MODULE_ID,     .name = "gps",     .cmd = &umdk_gps_command,        .reply = &umdk_gps_reply      },
-    { .id = UNWDS_LSM6DS3_MODULE_ID, .name = "lsm6ds3", .cmd = NULL,                     .reply = NULL                 },
-    { .id = UNWDS_LM75_MODULE_ID,    .name = "lm75",    .cmd = NULL,                     .reply = NULL                 },
-    { .id = UNWDS_LMT01_MODULE_ID,   .name = "lmt01",   .cmd = &umdk_lmt01_command,      .reply = &umdk_lmt01_reply    },
-    { .id = UNWDS_UART_MODULE_ID,    .name = "uart",    .cmd = &umdk_uart_command,       .reply = &umdk_uart_reply     },
-    { .id = UNWDS_SHT21_MODULE_ID,   .name = "sht21",   .cmd = &umdk_sht21_command,      .reply = &umdk_sht21_reply    },
-    { .id = UNWDS_PIR_MODULE_ID,     .name = "pir",     .cmd = NULL,                     .reply = &umdk_pir_reply      },
-    { .id = UNWDS_ADC_MODULE_ID,     .name = "adc",     .cmd = &umdk_adc_command,        .reply = &umdk_adc_reply      },
-    { .id = UNWDS_LPS331_MODULE_ID,  .name = "lps331",  .cmd = &umdk_lps331_command,     .reply = &umdk_lps331_reply   },
-    { .id = UNWDS_RSSIECHO_MODULE_ID,.name = "echo",    .cmd = &umdk_rssiecho_command,   .reply = &umdk_rssiecho_reply },
-    { .id = UNWDS_PWM_MODULE_ID,     .name = "pwm",     .cmd = &umdk_pwm_command,        .reply = &umdk_pwm_reply      },
-    { .id = UNWDS_OPT3001_MODULE_ID, .name = "opt3001", .cmd = &umdk_opt3001_command,    .reply = &umdk_opt3001_reply  },
-    { .id = UNWDS_DALI_MODULE_ID,    .name = "dali",    .cmd = NULL,                     .reply = NULL                 },
-    { .id = UNWDS_BME280_MODULE_ID,  .name = "bme280",  .cmd = &umdk_bme280_command,     .reply = &umdk_bme280_reply   },
-    { .id = UNWDS_M200_MODULE_ID,    .name = "m200",    .cmd = &umdk_m200_command,       .reply = &umdk_m200_reply     },
-    { .id = UNWDS_CONFIG_MODULE_ID,  .name = "config",  .cmd = &umdk_config_command,     .reply = &umdk_config_reply   },
-    { .id = UNWDS_PULSE_MODULE_ID,   .name = "pulse",   .cmd = &umdk_pulse_command,      .reply = &umdk_pulse_reply    },
-    { .id = UNWDS_MHZ19_MODULE_ID,   .name = "mhz19",   .cmd = &umdk_mhz19_command,      .reply = &umdk_mhz19_reply    },
-    { .id = UNWDS_IBUTTON_MODULE_ID, .name = "ibutton", .cmd = &umdk_ibutton_command,    .reply = &umdk_ibutton_reply  },
-    { .id = UNWDS_SWITCH_MODULE_ID,  .name = "switch",  .cmd = &umdk_switch_command,     .reply = &umdk_switch_reply   },
-    { .id = UNWDS_M230_MODULE_ID, 	 .name = "m230",  	.cmd = &umdk_m230_command,     	 .reply = &umdk_m230_reply     },
-    { .id = UNWDS_ADXL345_MODULE_ID, .name = "adxl345",	.cmd = &umdk_adxl345_command,  	 .reply = &umdk_adxl345_reply  },
-    { .id = UNWDS_IEC61107_MODULE_ID,.name = "iec61107",.cmd = &umdk_iec61107_command,   .reply = &umdk_iec61107_reply },
-    { .id = UNWDS_HD44780_MODULE_ID, .name = "hd44780",	.cmd = &umdk_hd44780_command,    .reply = &umdk_hd44780_reply  },
-    { .id = UNWDS_PACS_MODULE_ID,    .name = "pacs",	.cmd = &umdk_pacs_command,       .reply = &umdk_pacs_reply     },
-    { .id = UNWDS_IDCARD_MODULE_ID,  .name = "idcard",  .cmd = &umdk_idcard_command,     .reply = &umdk_idcard_reply   },
+    { .id = UNWDS_GPIO_MODULE_ID,      .name = "gpio",      .cmd = &umdk_gpio_command,       .reply = &umdk_gpio_reply      },
+    { .id = UNWDS_4BTN_MODULE_ID,      .name = "4btn",      .cmd = NULL,                     .reply = &umdk_4btn_reply      },
+    { .id = UNWDS_COUNTER_MODULE_ID,   .name = "counter",   .cmd = &umdk_counter_command,    .reply = &umdk_counter_reply   },
+    { .id = UNWDS_GPS_MODULE_ID,       .name = "gps",       .cmd = &umdk_gps_command,        .reply = &umdk_gps_reply       },
+    { .id = UNWDS_LSM6DS3_MODULE_ID,   .name = "lsm6ds3",   .cmd = NULL,                     .reply = NULL                  },
+    { .id = UNWDS_LM75_MODULE_ID,      .name = "lm75",      .cmd = NULL,                     .reply = NULL                  },
+    { .id = UNWDS_LMT01_MODULE_ID,     .name = "lmt01",     .cmd = &umdk_lmt01_command,      .reply = &umdk_lmt01_reply     },
+    { .id = UNWDS_UART_MODULE_ID,      .name = "uart",      .cmd = &umdk_uart_command,       .reply = &umdk_uart_reply      },
+    { .id = UNWDS_SHT21_MODULE_ID,     .name = "sht21",     .cmd = &umdk_sht21_command,      .reply = &umdk_sht21_reply     },
+    { .id = UNWDS_PIR_MODULE_ID,       .name = "pir",       .cmd = NULL,                     .reply = &umdk_pir_reply       },
+    { .id = UNWDS_ADC_MODULE_ID,       .name = "adc",       .cmd = &umdk_adc_command,        .reply = &umdk_adc_reply       },
+    { .id = UNWDS_LPS331_MODULE_ID,    .name = "lps331",    .cmd = &umdk_lps331_command,     .reply = &umdk_lps331_reply    },
+    { .id = UNWDS_RSSIECHO_MODULE_ID,  .name = "echo",      .cmd = &umdk_rssiecho_command,   .reply = &umdk_rssiecho_reply  },
+    { .id = UNWDS_PWM_MODULE_ID,       .name = "pwm",       .cmd = &umdk_pwm_command,        .reply = &umdk_pwm_reply       },
+    { .id = UNWDS_OPT3001_MODULE_ID,   .name = "opt3001",   .cmd = &umdk_opt3001_command,    .reply = &umdk_opt3001_reply   },
+    { .id = UNWDS_DALI_MODULE_ID,      .name = "dali",      .cmd = NULL,                     .reply = NULL                  },
+    { .id = UNWDS_BME280_MODULE_ID,    .name = "bme280",    .cmd = &umdk_bme280_command,     .reply = &umdk_bme280_reply    },
+    { .id = UNWDS_M200_MODULE_ID,      .name = "m200",      .cmd = &umdk_m200_command,       .reply = &umdk_m200_reply      },
+    { .id = UNWDS_CONFIG_MODULE_ID,    .name = "config",    .cmd = &umdk_config_command,     .reply = &umdk_config_reply    },
+    { .id = UNWDS_PULSE_MODULE_ID,     .name = "pulse",     .cmd = &umdk_pulse_command,      .reply = &umdk_pulse_reply     },
+    { .id = UNWDS_MHZ19_MODULE_ID,     .name = "mhz19",     .cmd = &umdk_mhz19_command,      .reply = &umdk_mhz19_reply     },
+    { .id = UNWDS_IBUTTON_MODULE_ID,   .name = "ibutton",   .cmd = &umdk_ibutton_command,    .reply = &umdk_ibutton_reply   },
+    { .id = UNWDS_SWITCH_MODULE_ID,    .name = "switch",    .cmd = &umdk_switch_command,     .reply = &umdk_switch_reply    },
+    { .id = UNWDS_M230_MODULE_ID,      .name = "m230",      .cmd = &umdk_m230_command,       .reply = &umdk_m230_reply      },
+    { .id = UNWDS_ADXL345_MODULE_ID,   .name = "adxl345",   .cmd = &umdk_adxl345_command,    .reply = &umdk_adxl345_reply   },
+    { .id = UNWDS_IEC61107_MODULE_ID,  .name = "iec61107",  .cmd = &umdk_iec61107_command,   .reply = &umdk_iec61107_reply  },
+    { .id = UNWDS_HD44780_MODULE_ID,   .name = "hd44780",   .cmd = &umdk_hd44780_command,    .reply = &umdk_hd44780_reply   },
+    { .id = UNWDS_PACS_MODULE_ID,      .name = "pacs",      .cmd = &umdk_pacs_command,       .reply = &umdk_pacs_reply      },
+    { .id = UNWDS_IDCARD_MODULE_ID,    .name = "idcard",    .cmd = &umdk_idcard_command,     .reply = &umdk_idcard_reply    },
+    { .id = UNWDS_IRBLASTER_MODULE_ID, .name = "irblaster", .cmd = &umdk_irblaster_command,  .reply = &umdk_irblaster_reply },
 };
 
 bool (*umdk_reply_ptr)(uint8_t*, int, mqtt_msg_t*);
