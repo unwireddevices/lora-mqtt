f1504ce55df59ed73ea7dcdec8b81fa007bd6a5a
Oleg Artamonov <oleg@unwds.com>
2017-12-14 19:58:34 +0300

    utils.c: better byte-sex functions

--- include/utils.h
+++ include/utils.h
@@ -42,8 +42,13 @@ void bytes_to_hex(uint8_t *bytes, size_t num_bytes, char *str, bool reverse_orde
 
 bool is_big_endian(void);
 
-void uint32_to_le(uint32_t *num);
 void uint16_to_le(uint16_t *num);
+void uint32_to_le(uint32_t *num);
+void uint64_to_le(uint64_t *num);
+
+void uint16_swap_bytes(uint16_t *num);
+void uint32_swap_bytes(uint32_t *num);
+void uint64_swap_bytes(uint64_t *num);
 
 void logprint(char *str);
 
@@ -51,17 +56,6 @@ void int_to_float_str(char *buf, int decimal, uint8_t precision);
 
 bool is_number(char* str);
 
-typedef struct {
-	float latitude;
-	float longitude;
-    /*
-	char date[15];
-	char time[15];
-    */
-    bool valid;
-    bool ready;
-} gps_data_t;
-
-void parse_gps_data(gps_data_t *gps, uint8_t *data, bool decode_nmea);
+uint16_t crc16_arc(uint8_t *data, uint16_t len);
 
 #endif
