286ddeb81b741202cd6439e9775a72fd6cd6b2e4
Oleg Artamonov <oleg@unwds.com>
2018-03-22 18:59:01 +0300

    umdk-usound initial implementation

--- modules/include/unwds-modules.h
+++ modules/include/unwds-modules.h
@@ -66,6 +66,7 @@ bool umdk_idcard_reply(uint8_t *moddata, int moddatalen, mqtt_msg_t *mqtt_msg);
 bool umdk_irblaster_reply(uint8_t *moddata, int moddatalen, mqtt_msg_t *mqtt_msg);
 bool umdk_hx711_reply(uint8_t *moddata, int moddatalen, mqtt_msg_t *mqtt_msg);
 bool umdk_dali_reply(uint8_t *moddata, int moddatalen, mqtt_msg_t *mqtt_msg);
+bool umdk_usound_reply(uint8_t *moddata, int moddatalen, mqtt_msg_t *mqtt_msg);
 
 void umdk_counter_command(char *param, char *out, int bufsize);
 void umdk_pulse_command(char *param, char *out, int bufsize);
@@ -94,6 +95,7 @@ void umdk_idcard_command(char *param, char *out, int bufsize);
 void umdk_irblaster_command(char *param, char *out, int bufsize);
 void umdk_hx711_command(char *param, char *out, int bufsize);
 void umdk_dali_command(char *param, char *out, int bufsize);
+void umdk_usound_command(char *param, char *out, int bufsize);
 
 static const unwds_module_desc_t unwds_modules_list[] = {
     { .id = UNWDS_GPIO_MODULE_ID,      .name = "gpio",      .cmd = &umdk_gpio_command,       .reply = &umdk_gpio_reply      },
@@ -127,6 +129,7 @@ static const unwds_module_desc_t unwds_modules_list[] = {
     { .id = UNWDS_IDCARD_MODULE_ID,    .name = "idcard",    .cmd = &umdk_idcard_command,     .reply = &umdk_idcard_reply    },
     { .id = UNWDS_IRBLASTER_MODULE_ID, .name = "irblaster", .cmd = &umdk_irblaster_command,  .reply = &umdk_irblaster_reply },
     { .id = UNWDS_HX711_MODULE_ID,     .name = "hx711",     .cmd = &umdk_hx711_command,      .reply = &umdk_hx711_reply     },
+    { .id = UNWDS_USOUND_MODULE_ID,    .name = "usound",    .cmd = &umdk_usound_command,     .reply = &umdk_usound_reply    },
 };
 
 bool (*umdk_reply_ptr)(uint8_t*, int, mqtt_msg_t*);
--- mqtt.c
+++ mqtt.c
@@ -46,7 +46,7 @@
 #include "unwds-mqtt.h"
 #include "utils.h"
 
-#define VERSION "2.2.2"
+#define VERSION "2.2.4"
 
 #define MAX_PENDING_NODES 1000
 
